b05ced66e62549c7e25d043791d362977b62049b
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2024-05-27 22:37:57 +0100

    Limit max tile prob to 0.8

--- SetupTileProbabilities.gd
+++ SetupTileProbabilities.gd
@@ -14,7 +14,7 @@ func create_probability_array(random_seed, grid_size_x, grid_size_y):
 	for x in range(grid_size_x):
 		for y in range(grid_size_y):
 			TileSetupDic [str(Vector2(x, y))] = {
-				"Prob": stepify(rng.randf(), 0.01),
+				"Prob": stepify(rng.randf_range(0.0, 0.8), 0.01),
 				"Times_Dug": 0.0, # float for later division
 				"Times_Success": 0.0, # float for later division
 				"Prob_Observed": 0.0,
