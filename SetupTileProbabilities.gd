ac4106d5c30c8fe90a6a0d1343156f5708c5f239
Sammi Rosser <29951987+Bergam0t@users.noreply.github.com>
2024-05-27 22:45:02 +0100

    Ensure at least 2 tiles will have prbability > 0.2

--- SetupTileProbabilities.gd
+++ SetupTileProbabilities.gd
@@ -10,16 +10,28 @@ func create_probability_array(random_seed, grid_size_x, grid_size_y):
 	
 	var TileSetupDic = {}
 	
+	var p = 0.0
+	
+	var p_above_min_count = 0
+	
 	# Set up an array that will store the probabilities
-	for x in range(grid_size_x):
-		for y in range(grid_size_y):
-			TileSetupDic [str(Vector2(x, y))] = {
-				"Prob": stepify(rng.randf_range(0.0, 0.8), 0.01),
-				"Times_Dug": 0.0, # float for later division
-				"Times_Success": 0.0, # float for later division
-				"Prob_Observed": 0.0,
-				"Prob_Estimate": rng.randf()
-			}
+	
+	
+	while p_above_min_count < 2:
+		p_above_min_count = 0
+		for x in range(grid_size_x):
+			for y in range(grid_size_y):
+				p = stepify(rng.randf_range(0.0, 0.8), 0.01)
+				if p > 0.2:
+					p_above_min_count += 1
+				
+				TileSetupDic [str(Vector2(x, y))] = {
+					"Prob": p,
+					"Times_Dug": 0.0, # float for later division
+					"Times_Success": 0.0, # float for later division
+					"Prob_Observed": 0.0, # float for later division
+					"Prob_Estimate": rng.randf() # random float between 0 and 1
+				}
 		
 	return TileSetupDic
 	
